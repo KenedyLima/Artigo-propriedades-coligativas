b1bcf1f053f6b7833bbafab8ffc633eac631f32e
kenedyLima <kenedytorreslima@gmail.com>
2022-05-12 23:04:26 -0300

    commit

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const experimentoVideos = document.querySelector(".experimentoVideos");
-const buttons = document.querySelectorAll(".button");
-const videoCounter = 0;
-
-buttons.forEach((element) => {
-  element.addEventListener("click", function (e) {
-    const button = e.target.closest(".button");
-    if (button) {
-      const videoNumber = Number(button.id);
-      console.log(button.id);
-    }
-  });
-});
 
 window.addEventListener("load", function () {
   this.alert(
