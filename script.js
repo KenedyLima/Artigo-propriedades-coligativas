bfbfe709dee546617aa7460435428cb9e353c327
kenedyLima <kenedytorreslima@gmail.com>
2022-05-12 23:58:39 -0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-window.addEventListener("load", function () {
-  this.alert(
-    "Turma 122, Edificações. Grupo composto por: Beatriz Pimentel, Amanda Batista, Layla Araújo, Jemima Rayssa  e Jeferson Kenedy."
-  );
-});
+// window.addEventListener("load", function () {
+//   this.alert(
+//     "Turma 122, Edificações. Grupo composto por: Beatriz Pimentel, Amanda Batista, Layla Araújo, Jemima Rayssa  e Jeferson Kenedy."
+//   );
+// });
